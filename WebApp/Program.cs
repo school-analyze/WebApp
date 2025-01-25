ac7c61e975982c1a138cb5a3aa3aa3ca9ae92e1a
Rohozin Maksym <rohozin.max@gmail.com>
2025-01-25 11:14:27 +0100

    Added MudBlazor

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,4 +1,5 @@
 using WebApp.Components;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddMudServices();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
