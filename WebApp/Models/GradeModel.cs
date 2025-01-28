9789812bd5505aa947de093e46dc41f0cbd75b8f
Rohozin Maksym <rohozin.max@gmail.com>
2025-01-28 11:57:23 +0100

    Created grade models and table in home

---
+++ WebApp/Models/GradeModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApp.Models;
+
+public class GradeModel
+{
+    [Key]
+    public int id { get; set; }
+    [Required]
+    [ForeignKey(nameof(UserModel))]
+    public int userId { get; set; }
+    [Required]
+    public Subject subject { get; set; }
+    [Required]
+    [Range(0.00, 10.00)]
+    public decimal grade { get; set; }
+    [Required]
+    [Range(0, 100)]
+    public int percentageInfluence { get; set; } = 100;
+    [Required]
+    public DateOnly dateAdded { get; set; }
+}
---
+++ WebApp/Models/Subject.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApp.Models;
+
+public enum Subject
+{
+    Math,
+    Italian,
+    History,
+    PhysicalEducation,
+    SIR,
+    English,
+    Telecommunication,
+    TPI,
+    ComputerScience,
+    Religion
+}
